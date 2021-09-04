24c8e39b56598a56a3c06c5d27f407d5968ae8cf
addo <addo.neto@gmail.com>
2021-09-04 14:28:27 -0300

    promise based request + code clean up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const https = require('https');
 const Twitter =  require('twitter');
+const https = require('https');
 require('dotenv').config();
 
 const HOST = 'saopaulo.sp.gov.br';
@@ -17,57 +17,55 @@ const client = new Twitter({
 
 let previousData = [];
 
-function fetchData(date){
-    let month = (date.getMonth() + 1).toString();
-    let day  = date.getDate().toString();
+function getData(date){
+    let month = (date.getMonth()  + 1).toString();
+    let day   = (date.getDate()   - 1).toString();
 
-    if(month.length === 1) month = '0' + month;
-    if(day.length === 1) day = '0' + day;
+    month = month.length === 1 ? '0' + month : month;
+    day   = day.length === 1   ? '0' + day : day;
 
     const options = {
         hostname: HOST,
-        method: 'GET',
+        method:   'GET',
         path: `/wp-content/uploads/2021/${month}/2021${month}${day}_vacinometro.csv`
     }
 
-    let response;
+    return new Promise((resolve, reject) => {
+        let csvData;
 
-    // https://nodejs.dev/learn/making-http-requests-with-nodejs
-    const req = https.request(options, res => {
-        res.on('data', data => response += data)
-        res.on('end', () => parseData(response))
+        const request = https.request(options, res => {
+            res.on('data', data => csvData += data);
+            res.on('end' , ()   => { 
+                resolve(csvData)
+                console.log('\x1b[32m%s\x1b[0m',
+                    'Data fetched from: \n' + HOST + options.path);
+            });
+        });
+
+        request.on('error', err => reject(err));
+        request.end();
     });
-
-    console.log('FETCH: ' + HOST + options.path);
-
-    req.on('error', err => console.error(err))
-    req.end();
 }
 
-function parseData(response){
-    let lastIndex = 0;
-    let data = [];
+function parseData(resp){
+    let lastIndex = 0, data = [], finalData = [];
 
-    while(lastIndex != -1){
-        lastIndex = response.indexOf("ARARAQUARA", lastIndex + 1);
-        if(lastIndex === -1) break
-        data.push(response.substr(lastIndex, 25));
+    while(lastIndex != -1) {
+        lastIndex = resp.indexOf("ARARAQUARA", lastIndex + 1);
+        if(lastIndex === -1) break;
+        data.push(resp.substr(lastIndex, 25));
     }
 
-    let finalData = [];
-
     for(let d of data){
-        // regex ?
-
         let newD = d.split(";");
-        newD = newD[newD.length-1].split('\r');
+        newD = newD[newD.length - 1].split('\r');
         finalData.push(newD[0]);
     }
 
-    calculateVaccination(finalData);
+    calcVaccinationRates(finalData);
 }
 
-function calculateVaccination(data){
+function calcVaccinationRates(data){
     singleDose = Number(data[0]);
     firstDose  = Number(data[2]);
     secondDose = Number(data[1]);
@@ -78,7 +76,7 @@ function calculateVaccination(data){
     fstDPercent = (firstDose  * 100 / POPULATION).toFixed(2);
     sndDPercent = (secondDose * 100 / POPULATION).toFixed(2);
 
-    updateStatus(sglDPercent, fstDPercent, sndDPercent, singleDose, firstDose, secondDose);
+    updateStatus(sglDPercent, fstDPercent, sndDPercent);
 }
 
 function isNewData(s, fst, snd){
@@ -96,40 +94,38 @@ function isNewData(s, fst, snd){
     return true;
 }
 
-function generateProgressBar(percent){
+function generateProgressBar(p){
     let finalBar = '';
-    let fill = Math.trunc(percent / PERCENT_GAP);
+    let fill = Math.trunc(p / PERCENT_GAP);
 
-    for(let i = 0; i < fill; i++){
-        finalBar += CHARS[0];
-    }
+    for(let i = 0; i < fill; i++) finalBar += CHARS[0];
 
-    if(percent - fill * 5 > 0){
+    if(p - fill * PERCENT_GAP > 0){
         fill++;
         finalBar += CHARS[1];
     }
 
-    for(let i = 0; i < BAR_SIZE - fill; i++) {
-        finalBar += CHARS[2];
-    }
+    for(let i = 0; i < BAR_SIZE - fill; i++) finalBar += CHARS[2];
      
     return finalBar;
 }
 
-function updateStatus(sglDpct, fstDpct, sndDpct, sglDose, fstDose, sndDose){
+function updateStatus(sglDpct, fstDpct, sndDpct){
 
     let total = (Number(sndDpct) + Number(sglDpct)).toFixed(2);
 
-    let msg = `Totalmente Vacinados\n${generateProgressBar(total)} ${total}%\n\nPrimeira Dose\n${generateProgressBar(fstDpct)} ${fstDpct}%\n\nSegunda Dose\n${generateProgressBar(sndDpct)} ${sndDpct}%\n\nDose Única\n${generateProgressBar(sglDpct)} ${sglDpct}%`
+    let msg = `Totalmente Vacinados\n${generateProgressBar(total)} ${total}%\n\nPrimeira Dose\n${generateProgressBar(fstDpct)} ${fstDpct}%\n\nSegunda Dose\n${generateProgressBar(sndDpct)} ${sndDpct}%\n\nDose Única\n${generateProgressBar(sglDpct)} ${sglDpct}%`;
 
     client.post('statuses/update', { status: msg })
-        .then(r =>  console.log(`You successfully tweeted this : ${r.text}`))
+        .then(r =>  console.log('\x1b[36mm%s\x1b[0m', `Status Updated!`))
         .catch(console.error);
 }
 
 function update(){
-    fetchData(new Date());
-    setInterval(update, 1000 * 60 * 60 * 24);
+    getData(new Date())
+        .then(result => parseData(result))
+        .catch(err   => console.error(err));
 }
 
 update();
+setInterval(update, 1000 * 60 * 60 * 24);
